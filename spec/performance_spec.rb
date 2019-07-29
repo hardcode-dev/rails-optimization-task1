0b59ecf16643154142e90cb7bbb7e7f19db81335
Pavel Trepkosh <mrlewap@gmail.com>
2019-07-29 22:11:10 +0600

    update performance spec

--- spec/performance_spec.rb
+++ spec/performance_spec.rb
@@ -1,8 +1,15 @@
 require 'rspec-benchmark'
 require_relative '../work_method'
 
+RSpec::Benchmark.configure do |config|
+  config.disable_gc = true
+  config.samples = 3
+end
+
 RSpec.describe "Performance testing" do
   include RSpec::Benchmark::Matchers
 
-  it { expect { work('data_large.txt', 16000) }.to perform_under(340).ms } # Goal => 147 and linear
+  it { expect { work('data_large.txt', 16000) }.to perform_under(160).ms } # Goal => 147 and linear
+
+  it { expect { |n, _i| work('data_large.txt', n) }.to perform_linear.in_range(1000, 32_000).ratio(2) }
 end

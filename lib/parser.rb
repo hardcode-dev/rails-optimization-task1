bd286f7f8220993d277c8355146a8acba906a63a
Denis Nazmutdinov <w.nazmutdinov@gmail.com>
2019-08-02 22:13:25 +0300

    [Task-1] Collect user info in one loop instead of multiple

--- lib/parser.rb
+++ lib/parser.rb
@@ -30,12 +30,41 @@ class Parser
     }
   end
 
-  def collect_stats_from_users(report, users_objects)
-    users_objects.each do |user|
-      user_key = "#{user.attributes['first_name']}" + ' ' + "#{user.attributes['last_name']}"
+  def collect_stats_from_users(report, users)
+    users.each do |user|
+      user_key = "#{user.attributes['first_name']} #{user.attributes['last_name']}"
       report['usersStats'][user_key] ||= {}
-      report['usersStats'][user_key] = report['usersStats'][user_key].merge(yield(user))
+
+      # Собираем количество сессий по пользователям
+      report['usersStats'][user_key]['sessionsCount'] = user.sessions.count
+
+      # Собираем количество времени по пользователям
+      total_time = user.sessions.map {|s| s['time']}
+      total_time.map! {|t| t.to_i}
+      report['usersStats'][user_key]['totalTime'] = total_time.sum.to_s + ' min.'
+
+      # Выбираем самую длинную сессию пользователя
+      longest_session = user.sessions.map {|s| s['time']}
+      longest_session.map! {|t| t.to_i}
+      report['usersStats'][user_key]['longestSession'] = longest_session.max.to_s + ' min.'
+
+      # Браузеры пользователя через запятую
+      browsers = user.sessions.map {|s| s['browser']}
+      browsers.map! {|b| b.upcase}.sort!
+      report['usersStats'][user_key]['browsers'] = browsers.join(', ')
+
+      # Хоть раз использовал IE?
+      report['usersStats'][user_key]['usedIE'] = user.sessions.map{|s| s['browser']}.any? { |b| b.upcase =~ /INTERNET EXPLORER/ }
+
+      # Всегда использовал только Chrome?
+      report['usersStats'][user_key]['alwaysUsedChrome'] = user.sessions.map{|s| s['browser']}.all? { |b| b.upcase =~ /CHROME/ }
+
+      # Даты сессий через запятую в обратном порядке в формате iso8601
+      dates = user.sessions.map {|s| s['date']}
+      dates.map! {|d| Date.parse(d)}.sort!.reverse!.map! { |d| d.iso8601 }
+      report['usersStats'][user_key]['dates'] = dates
     end
+    report
   end
 
   def work
@@ -43,14 +72,14 @@ class Parser
 
     sessions = {}
     all_sessions = []
-    users_objects = []
+    users = []
     uniqueBrowsers = Set.new
 
     file_lines.each do |line|
       cols = line.split(',')
 
       if cols[0] == 'user'
-        users_objects << User.new(attributes: parse_user(cols))
+        users << User.new(attributes: parse_user(cols))
       elsif cols[0] == 'session'
         session = parse_session(cols)
         sessions[session['user_id']] ||= []
@@ -60,7 +89,7 @@ class Parser
       end
     end
 
-    users_objects.each do |user|
+    users.each do |user|
       user.sessions = sessions[user.attributes['id']] || []
     end
 
@@ -81,7 +110,7 @@ class Parser
 
     report = {}
 
-    report[:totalUsers] = users_objects.count
+    report[:totalUsers] = users.count
 
     report['uniqueBrowsersCount'] = uniqueBrowsers.count
 
@@ -94,49 +123,8 @@ class Parser
     # Статистика по пользователям
 
     report['usersStats'] = {}
+    report = collect_stats_from_users(report, users)
 
-    # Собираем количество сессий по пользователям
-    collect_stats_from_users(report, users_objects) do |user|
-      { 'sessionsCount' => user.sessions.count }
-    end
-
-    # Собираем количество времени по пользователям
-    collect_stats_from_users(report, users_objects) do |user|
-      total_time = user.sessions.map {|s| s['time']}
-      total_time.map! {|t| t.to_i}
-      { 'totalTime' => total_time.sum.to_s + ' min.' }
-    end
-
-    # Выбираем самую длинную сессию пользователя
-    collect_stats_from_users(report, users_objects) do |user|
-      longest_session = user.sessions.map {|s| s['time']}
-      longest_session.map! {|t| t.to_i}
-      { 'longestSession' => longest_session.max.to_s + ' min.' }
-    end
-
-    # Браузеры пользователя через запятую
-    collect_stats_from_users(report, users_objects) do |user|
-      browsers = user.sessions.map {|s| s['browser']}
-      browsers.map! {|b| b.upcase}.sort!
-      { 'browsers' => browsers.join(', ') }
-    end
-
-    # Хоть раз использовал IE?
-    collect_stats_from_users(report, users_objects) do |user|
-      { 'usedIE' => user.sessions.map{|s| s['browser']}.any? { |b| b.upcase =~ /INTERNET EXPLORER/ } }
-    end
-
-    # Всегда использовал только Chrome?
-    collect_stats_from_users(report, users_objects) do |user|
-      { 'alwaysUsedChrome' => user.sessions.map{|s| s['browser']}.all? { |b| b.upcase =~ /CHROME/ } }
-    end
-
-    # Даты сессий через запятую в обратном порядке в формате iso8601
-    collect_stats_from_users(report, users_objects) do |user|
-      dates = user.sessions.map {|s| s['date']}
-      dates.map! {|d| Date.parse(d)}.sort!.reverse!.map! { |d| d.iso8601 }
-      { 'dates' => dates }
-    end
 
     File.write('result.json', "#{report.to_json}\n")
   end
--- profiling/prof.rb
+++ profiling/prof.rb
@@ -2,29 +2,32 @@ require 'benchmark'
 require 'ruby-prof'
 require_relative '../lib/parser'
 
-# Benchmark.bm do |x|
-#   x.report { Parser.new(file_path: '../200000.txt').work }
-#   x.report { Parser.new(file_path: '../200000.txt').work }
-#   x.report { Parser.new(file_path: '../200000.txt').work }
-# end
-
-RubyProf.measure_mode = RubyProf::WALL_TIME
-
-result = RubyProf.profile do
-  Parser.new(file_path: '../200000.txt').work
+lines = 200000
+`head -#{lines} ../data_large.txt > profiling/#{lines}.txt`
+puts "#{lines} lines:"
+Benchmark.bm do |x|
+  x.report { Parser.new(file_path: "profiling/#{lines}.txt").work }
+  x.report { Parser.new(file_path: "profiling/#{lines}.txt").work }
+  x.report { Parser.new(file_path: "profiling/#{lines}.txt").work }
 end
 
-printer = RubyProf::FlatPrinter.new(result)
-printer.print(File.open("profiling/flat.txt", "w+"))
-
+# RubyProf.measure_mode = RubyProf::WALL_TIME
+#
+# result = RubyProf.profile do
+#   Parser.new(file_path: '../200000.txt').work
+# end
+#
+# printer = RubyProf::FlatPrinter.new(result)
+# printer.print(File.open("profiling/flat.txt", "w+"))
+#
+# printer = RubyProf::GraphHtmlPrinter.new(result)
+# printer.print(File.open("profiling/graph.html", "w+"))
+#
+# printer = RubyProf::CallStackPrinter.new(result)
+# printer.print(File.open("profiling/callstack.html", "w+"))
+#
 # printer = RubyProf::GraphPrinter.new(result)
 # printer.print(File.open("profiling/graph.txt", "w+"))
-
-printer = RubyProf::GraphHtmlPrinter.new(result)
-printer.print(File.open("profiling/graph.html", "w+"))
-
-printer = RubyProf::CallStackPrinter.new(result)
-printer.print(File.open("profiling/callstack.html", "w+"))
-
+#
 # printer = RubyProf::CallStackPrinter.new(result)
 # printer.print(File.open("profiling/calltree.callgrind", "w+"))

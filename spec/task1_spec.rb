2df5551d1b809f237b88d89619799290483f4041
Aleh <bisnomen@gmail.com>
2019-08-09 09:06:04 +0300

    replace performace fixture with 100k

--- spec/task1_spec.rb
+++ spec/task1_spec.rb
@@ -6,7 +6,7 @@ describe 'Task1' do
   let(:data_file_path) { 'spec/fixtures/data.txt' }
   let(:output_file_path) { 'result.json' }
   let(:expected_output) { File.read('spec/fixtures/expected_output.json') }
-  let(:data_file_path_10k) { 'spec/fixtures/10_000_lines.txt' }
+  let(:data_file_path_100k) { 'spec/fixtures/100_000_lines.txt' }
 
   before { File.delete(output_file_path) if File.exists?(output_file_path) }
   after { File.delete(output_file_path) if File.exists?(output_file_path) }
@@ -17,7 +17,7 @@ describe 'Task1' do
   end
 
   it 'mets performance' do
-    expect { work(data_file_path_10k) }.to perform_under(180).ms
+    expect { work(data_file_path_100k) }.to perform_under(2200).ms
   end
 end
 

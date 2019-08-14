c26c2d7c6f963a89020b47145d2c11044ef45f93
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2019-08-14 15:36:53 +0500

    [task1] add report and test for task-1

---
+++ codes/report.rb
@@ -0,0 +1,86 @@
+require 'fileutils'
+require_relative "task-1"
+
+STEP = '1'
+
+class Report
+  attr_reader :type, :step, :folder
+
+  def initialize(type)
+    @type = type
+    @step = STEP
+    @folder = type.to_s
+  end
+
+  def full_name
+    File.join(path, filename)
+  end
+
+  def file
+    File.open(full_name, "w+")
+  end
+
+  def run
+    work(data, disable_gc: true)
+    puts command if type != :test
+  end
+
+  def save(body)
+    file.write(body)
+    body
+  end
+
+  private
+
+  def command
+    action = case type
+             when :stackprof
+               "stackprof"
+             else
+               "open"
+             end
+    "#{action} #{full_name}"
+  end
+
+  def extension
+    case type
+    when :graph, :callstack
+      ".html"
+    when :stackprof
+      ".dump"
+    else
+      ".txt"
+    end
+  end
+
+  def data
+    File.join('data', "#{data_filename}.txt")
+  end
+
+  def version
+    File.read('.ruby-version').strip
+  end
+
+  def path
+    path = File.join("reports", version, data_filename, folder)
+    FileUtils.mkdir_p(path) unless Dir.exist? path
+    path
+  end
+
+  def filename
+    "#{step}#{extension}"
+  end
+
+  def data_filename
+    case type
+    when :test
+      "test"
+    else
+      #"100_000"
+      #"800_000"
+      #"1_600_000"
+      #"large"
+      "small"
+    end
+  end
+end

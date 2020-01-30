4b223b655b2c6555e7ab4c9d29ccbafc6b16ef88
kortirso <kortirso@gmail.com>
2020-01-30 16:04:15 +0300

    using ruby-prof Flat mode for Array#select optimisation

---
+++ script.rb
@@ -0,0 +1,12 @@
+require 'ruby-prof'
+require_relative 'task-1.rb'
+
+RubyProf.measure_mode = RubyProf::WALL_TIME
+
+result = RubyProf.profile do
+  work(filename: 'data_10000.txt', disable_gc: true)
+end
+
+printer = RubyProf::FlatPrinter.new(result)
+
+printer.print(File.open('ruby_prof_reports/flat.txt', 'w+'))

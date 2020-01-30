c1d9a0672588ccdaae93860ef21e20af72ea1053
Konstantin Bludov <konstantin.bludov@binomo.com>
2020-01-30 10:35:58 +0300

    Rubocop simple raid

--- task-1.rb
+++ task-1.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Deoptimized version of homework task
 
 require 'json'
@@ -20,7 +22,7 @@ def parse_user(user)
     'id' => fields[1],
     'first_name' => fields[2],
     'last_name' => fields[3],
-    'age' => fields[4],
+    'age' => fields[4]
   }
 end
 
@@ -31,13 +33,13 @@ def parse_session(session)
     'session_id' => fields[2],
     'browser' => fields[3],
     'time' => fields[4],
-    'date' => fields[5],
+    'date' => fields[5]
   }
 end
 
 def collect_stats_from_users(report, users_objects, &block)
   users_objects.each do |user|
-    user_key = "#{user.attributes['first_name']}" + ' ' + "#{user.attributes['last_name']}"
+    user_key = user.attributes['first_name'].to_s + ' ' + user.attributes['last_name'].to_s
     report['usersStats'][user_key] ||= {}
     report['usersStats'][user_key] = report['usersStats'][user_key].merge(block.call(user))
   end
@@ -51,8 +53,8 @@ def work
 
   file_lines.each do |line|
     cols = line.split(',')
-    users = users + [parse_user(line)] if cols[0] == 'user'
-    sessions = sessions + [parse_session(line)] if cols[0] == 'session'
+    users += [parse_user(line)] if cols[0] == 'user'
+    sessions += [parse_session(line)] if cols[0] == 'session'
   end
 
   # Отчёт в json
@@ -87,11 +89,11 @@ def work
 
   report['allBrowsers'] =
     sessions
-      .map { |s| s['browser'] }
-      .map { |b| b.upcase }
-      .sort
-      .uniq
-      .join(',')
+    .map { |s| s['browser'] }
+    .map(&:upcase)
+    .sort
+    .uniq
+    .join(',')
 
   # Статистика по пользователям
   users_objects = []
@@ -100,7 +102,7 @@ def work
     attributes = user
     user_sessions = sessions.select { |session| session['user_id'] == user['id'] }
     user_object = User.new(attributes: attributes, sessions: user_sessions)
-    users_objects = users_objects + [user_object]
+    users_objects += [user_object]
   end
 
   report['usersStats'] = {}
@@ -112,32 +114,32 @@ def work
 
   # Собираем количество времени по пользователям
   collect_stats_from_users(report, users_objects) do |user|
-    { 'totalTime' => user.sessions.map {|s| s['time']}.map {|t| t.to_i}.sum.to_s + ' min.' }
+    { 'totalTime' => user.sessions.map { |s| s['time'] }.map(&:to_i).sum.to_s + ' min.' }
   end
 
   # Выбираем самую длинную сессию пользователя
   collect_stats_from_users(report, users_objects) do |user|
-    { 'longestSession' => user.sessions.map {|s| s['time']}.map {|t| t.to_i}.max.to_s + ' min.' }
+    { 'longestSession' => user.sessions.map { |s| s['time'] }.map(&:to_i).max.to_s + ' min.' }
   end
 
   # Браузеры пользователя через запятую
   collect_stats_from_users(report, users_objects) do |user|
-    { 'browsers' => user.sessions.map {|s| s['browser']}.map {|b| b.upcase}.sort.join(', ') }
+    { 'browsers' => user.sessions.map { |s| s['browser'] }.map(&:upcase).sort.join(', ') }
   end
 
   # Хоть раз использовал IE?
   collect_stats_from_users(report, users_objects) do |user|
-    { 'usedIE' => user.sessions.map{|s| s['browser']}.any? { |b| b.upcase =~ /INTERNET EXPLORER/ } }
+    { 'usedIE' => user.sessions.map { |s| s['browser'] }.any? { |b| b.upcase =~ /INTERNET EXPLORER/ } }
   end
 
   # Всегда использовал только Chrome?
   collect_stats_from_users(report, users_objects) do |user|
-    { 'alwaysUsedChrome' => user.sessions.map{|s| s['browser']}.all? { |b| b.upcase =~ /CHROME/ } }
+    { 'alwaysUsedChrome' => user.sessions.map { |s| s['browser'] }.all? { |b| b.upcase =~ /CHROME/ } }
   end
 
   # Даты сессий через запятую в обратном порядке в формате iso8601
   collect_stats_from_users(report, users_objects) do |user|
-    { 'dates' => user.sessions.map{|s| s['date']}.map {|d| Date.parse(d)}.sort.reverse.map { |d| d.iso8601 } }
+    { 'dates' => user.sessions.map { |s| s['date'] }.map { |d| Date.parse(d) }.sort.reverse.map(&:iso8601) }
   end
 
   File.write('result.json', "#{report.to_json}\n")
@@ -147,25 +149,25 @@ class TestMe < Minitest::Test
   def setup
     File.write('result.json', '')
     File.write('data.txt',
-'user,0,Leida,Cira,0
-session,0,0,Safari 29,87,2016-10-23
-session,0,1,Firefox 12,118,2017-02-27
-session,0,2,Internet Explorer 28,31,2017-03-28
-session,0,3,Internet Explorer 28,109,2016-09-15
-session,0,4,Safari 39,104,2017-09-27
-session,0,5,Internet Explorer 35,6,2016-09-01
-user,1,Palmer,Katrina,65
-session,1,0,Safari 17,12,2016-10-21
-session,1,1,Firefox 32,3,2016-12-20
-session,1,2,Chrome 6,59,2016-11-11
-session,1,3,Internet Explorer 10,28,2017-04-29
-session,1,4,Chrome 13,116,2016-12-28
-user,2,Gregory,Santos,86
-session,2,0,Chrome 35,6,2018-09-21
-session,2,1,Safari 49,85,2017-05-22
-session,2,2,Firefox 47,17,2018-02-02
-session,2,3,Chrome 20,84,2016-11-25
-')
+               'user,0,Leida,Cira,0
+               session,0,0,Safari 29,87,2016-10-23
+               session,0,1,Firefox 12,118,2017-02-27
+               session,0,2,Internet Explorer 28,31,2017-03-28
+               session,0,3,Internet Explorer 28,109,2016-09-15
+               session,0,4,Safari 39,104,2017-09-27
+               session,0,5,Internet Explorer 35,6,2016-09-01
+               user,1,Palmer,Katrina,65
+               session,1,0,Safari 17,12,2016-10-21
+               session,1,1,Firefox 32,3,2016-12-20
+               session,1,2,Chrome 6,59,2016-11-11
+               session,1,3,Internet Explorer 10,28,2017-04-29
+               session,1,4,Chrome 13,116,2016-12-28
+               user,2,Gregory,Santos,86
+               session,2,0,Chrome 35,6,2018-09-21
+               session,2,1,Safari 49,85,2017-05-22
+               session,2,2,Firefox 47,17,2018-02-02
+               session,2,3,Chrome 20,84,2016-11-25
+               ')
   end
 
   def test_result

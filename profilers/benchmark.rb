2ecb238370f5c78d383a07e7852ed6744400bb79
Evgeniy Abramov <f1@oren.ru>
2020-02-01 13:50:32 +0500

    Step 5

--- profilers/benchmark.rb
+++ profilers/benchmark.rb
@@ -1,16 +1,22 @@
+# gem install kalibera
 require 'benchmark/ips'
 require_relative '../task-1.rb'
 
-gc = { disable_gc: true }
+ROWS = 100_000
+COLS = 10
+REPS = 1000
+
+GC.disable
 
 Benchmark.ips do |x|
-  # x.config(:stats => :bootstrap, :confidence => 99)
+  # The default is :stats => :sd, which doesn't have a configurable confidence
+  # confidence is 95% by default, so it can be omitted
+  x.config(
+    stats: :bootstrap,
+    confidence: 95,
+  )
 
-  x.report('100') { work('data_100.txt', gc) }
-
-  x.report('1_000') { work('data_1_000.txt', gc) }
-
-  x.report("10_000") { work('data_10_000.txt', gc) }
-
-  x.compare!
+  x.report("data_1_000") do
+    x.report('1_000') { work('data_1_000.txt', gc) }
+  end
 end

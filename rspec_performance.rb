e1b736b1f81d1fcbb554d667b61a8aa8aad87ab3
lokideos <archmage09@gmail.com>
2020-02-02 17:56:33 +0300

    Add simple rspec performance test to the project

---
+++ rspec_performance.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rspec-benchmark'
+require_relative 'task-1'
+require 'minitest/autorun'
+
+RSpec.configure do |config|
+  config.include RSpec::Benchmark::Matchers
+end
+
+describe 'Process test' do
+  it 'works under 2.4 seconds for 400 records' do
+    expect { work('rspec_test_data.txt') }.to perform_under(2).ms.warmup(2).times.sample(10).times
+  end
+end

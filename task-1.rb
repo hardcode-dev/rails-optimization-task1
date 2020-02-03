38e5538790005a31e17f9f487fd2bdc1679d0d8d
Evgeny Gorbachev <djonsilver@yandex.ru>
2020-02-03 15:36:04 +0300

    memoization gaved me 10% speedup
    fix

--- task-1.rb
+++ task-1.rb
@@ -50,8 +50,8 @@ def parse_session(fields)
   Session.new(
       fields[1].to_i,
       fields[2],
-      fields[3],
-      fields[4],
+      fields[3].upcase,
+      fields[4].to_i,
       # Date.strptime(fields[5]),
       # no speedup
       # Date.strptime(fields[5], '%Y-%m-%d'),
@@ -85,13 +85,11 @@ def parse_file(file_lines)
 end
 
 def unique_browsers(sessions)
-  sessions.map{|ss| ss["browser"]}.uniq
+  @unique_browsers ||= sessions.map{|ss| ss["browser"]}.uniq
 end
 
 def get_browsers(sessions)
-  sessions
-      .map { |s| s['browser'] }
-      .uniq
+  unique_browsers(sessions)
       .sort
       .join(',')
 end
@@ -112,7 +110,7 @@ def collect_stats(report, users_objects, sessions)
 
 
   users_objects.each do |user|
-    user_key = "#{user.attributes['first_name']}" + ' ' + "#{user.attributes['last_name']}"
+    user_key = "#{user.first_name}" + ' ' + "#{user.last_name}"
     report['usersStats'][user_key] ||= {}
     # Собираем количество сессий по пользователям
     sessions_count = { 'sessionsCount' => user.sessions.count }
@@ -140,7 +138,7 @@ def collect_stats(report, users_objects, sessions)
   end
 end
 
-def work(filename = 'data_large.txt', disable_gc: false)
+def work(filename = 'data_small.txt', disable_gc: false)
   puts 'Start work'
   GC.disable if disable_gc
 
@@ -173,7 +171,7 @@ end
 class TestMe < Minitest::Test
   def setup
     File.write('result.json', '')
-    File.write('data.txt',
+    File.write('data.txt~',
 'user,0,Leida,Cira,0
 session,0,0,Safari 29,87,2016-10-23
 session,0,1,Firefox 12,118,2017-02-27
@@ -196,7 +194,7 @@ session,2,3,Chrome 20,84,2016-11-25
   end
 
   def test_result
-    work
+    work 'data.txt.~'
     expected_result = '{"totalUsers":3,"uniqueBrowsersCount":14,"totalSessions":15,"allBrowsers":"CHROME 13,CHROME 20,CHROME 35,CHROME 6,FIREFOX 12,FIREFOX 32,FIREFOX 47,INTERNET EXPLORER 10,INTERNET EXPLORER 28,INTERNET EXPLORER 35,SAFARI 17,SAFARI 29,SAFARI 39,SAFARI 49","usersStats":{"Leida Cira":{"sessionsCount":6,"totalTime":"455 min.","longestSession":"118 min.","browsers":"FIREFOX 12, INTERNET EXPLORER 28, INTERNET EXPLORER 28, INTERNET EXPLORER 35, SAFARI 29, SAFARI 39","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-09-27","2017-03-28","2017-02-27","2016-10-23","2016-09-15","2016-09-01"]},"Palmer Katrina":{"sessionsCount":5,"totalTime":"218 min.","longestSession":"116 min.","browsers":"CHROME 13, CHROME 6, FIREFOX 32, INTERNET EXPLORER 10, SAFARI 17","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-04-29","2016-12-28","2016-12-20","2016-11-11","2016-10-21"]},"Gregory Santos":{"sessionsCount":4,"totalTime":"192 min.","longestSession":"85 min.","browsers":"CHROME 20, CHROME 35, FIREFOX 47, SAFARI 49","usedIE":false,"alwaysUsedChrome":false,"dates":["2018-09-21","2018-02-02","2017-05-22","2016-11-25"]}}}' + "\n"
     assert_equal expected_result, File.read('result.json')
   end

5d417267ab5afedb3a3def524bbb54bca39f8702
Roman Perminov <permidon@me.com>
2020-02-03 10:36:28 -0500

    wip

--- benchmark.rb
+++ benchmark.rb
@@ -4,7 +4,8 @@ require_relative 'task-1.rb'
 puts "Start"
 
 time = Benchmark.realtime do
-  work('data5000.txt', disable_gc: false)
+  work('data_large.txt', disable_gc: false)
+  # work('data5000.txt', disable_gc: false)
 end
 
 puts "Finish in #{time.round(2)}"

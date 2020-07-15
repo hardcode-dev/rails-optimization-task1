04021f964698ee70a6963730b899d00dd911f6b1
m.oleynik <m.oleynik@fun-box.ru>
2020-07-15 22:11:29 +0300

    Проиндексировал сессии для быстрого поиска сессей определенного юзера

--- task-1.rb
+++ task-1.rb
@@ -4,6 +4,8 @@ require 'json'
 require 'pry'
 require 'date'
 require 'minitest/autorun'
+require 'rspec'
+require 'rspec-benchmark'
 
 class User
   attr_reader :attributes, :sessions
@@ -43,8 +45,8 @@ def collect_stats_from_users(report, users_objects, &block)
   end
 end
 
-def work
-  file_lines = File.read('data.txt').split("\n")
+def work(filename)
+  file_lines = File.read(filename).split("\n")
 
   users = []
   sessions = []
@@ -96,12 +98,28 @@ def work
   # Статистика по пользователям
   users_objects = []
 
+  # предварительно проиндексировал
+  sessions_index = sessions.each_with_object({}) do |session, index|
+    index[session['user_id']] = [] unless index[session['user_id']]
+    index[session['user_id']] << session
+    index[session['user_id']]
+  end
+  # O(1)
   users.each do |user|
     attributes = user
-    user_sessions = sessions.select { |session| session['user_id'] == user['id'] }
+
+    id = user['id']
+    user_sessions = sessions_index[id]
     user_object = User.new(attributes: attributes, sessions: user_sessions)
     users_objects = users_objects + [user_object]
   end
+
+  # users.each do |user|
+  #   attributes = user
+  #   user_sessions = sessions.select { |session| session['user_id'] == user['id'] }
+  #   user_object = User.new(attributes: attributes, sessions: user_sessions)
+  #   users_objects = users_objects + [user_object]
+  # end
 
   report['usersStats'] = {}
 
@@ -169,8 +187,23 @@ session,2,3,Chrome 20,84,2016-11-25
   end
 
   def test_result
-    work
+    work('data.txt')
     expected_result = '{"totalUsers":3,"uniqueBrowsersCount":14,"totalSessions":15,"allBrowsers":"CHROME 13,CHROME 20,CHROME 35,CHROME 6,FIREFOX 12,FIREFOX 32,FIREFOX 47,INTERNET EXPLORER 10,INTERNET EXPLORER 28,INTERNET EXPLORER 35,SAFARI 17,SAFARI 29,SAFARI 39,SAFARI 49","usersStats":{"Leida Cira":{"sessionsCount":6,"totalTime":"455 min.","longestSession":"118 min.","browsers":"FIREFOX 12, INTERNET EXPLORER 28, INTERNET EXPLORER 28, INTERNET EXPLORER 35, SAFARI 29, SAFARI 39","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-09-27","2017-03-28","2017-02-27","2016-10-23","2016-09-15","2016-09-01"]},"Palmer Katrina":{"sessionsCount":5,"totalTime":"218 min.","longestSession":"116 min.","browsers":"CHROME 13, CHROME 6, FIREFOX 32, INTERNET EXPLORER 10, SAFARI 17","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-04-29","2016-12-28","2016-12-20","2016-11-11","2016-10-21"]},"Gregory Santos":{"sessionsCount":4,"totalTime":"192 min.","longestSession":"85 min.","browsers":"CHROME 20, CHROME 35, FIREFOX 47, SAFARI 49","usedIE":false,"alwaysUsedChrome":false,"dates":["2018-09-21","2018-02-02","2017-05-22","2016-11-25"]}}}' + "\n"
     assert_equal expected_result, File.read('result.json')
+  end
+end
+
+RSpec.configure do |config|
+  config.include RSpec::Benchmark::Matchers
+end
+
+describe 'Performance' do
+  describe 'linear work' do
+    let(:filename) { 'data10000.txt' }
+    it 'works under 1 ms' do
+      expect {
+        work(filename)
+      }.to perform_under(700).ms.warmup(2).times.sample(10).times
+    end
   end
 end

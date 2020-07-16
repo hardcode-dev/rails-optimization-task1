c8f1e3782d3fb99f1823ba45dcb2686fbf442ad9
Ilya Merkulov <imerkulov@me.com>
2020-07-16 17:49:00 +0300

    Remove String#split double calls

--- task-1.rb
+++ task-1.rb
@@ -14,8 +14,7 @@ class User
   end
 end
 
-def parse_user(user)
-  fields = user.split(',')
+def parse_user(fields)
   {
     id: fields[1],
     first_name: fields[2],
@@ -25,8 +24,7 @@ def parse_user(user)
   }
 end
 
-def parse_session(session)
-  fields = session.split(',')
+def parse_session(fields)
   {
     user_id: fields[1],
     session_id: fields[2],
@@ -58,11 +56,11 @@ def work(filename, disable_gc: false)
     cols = line.split(',')
 
     if cols[0] == 'user'
-      user = parse_user(line)
+      user = parse_user(cols)
       users << user
     end
     if cols[0] == 'session'
-      session = parse_session(line)
+      session = parse_session(cols)
       sessions << session
       user[:sessions] << session
       unique_browsers.add(session[:browser])

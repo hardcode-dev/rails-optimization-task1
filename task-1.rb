bf11a3a9571a4abe7a86e3ecd37558366c247e69
Ilya Merkulov <imerkulov@me.com>
2020-07-16 18:28:19 +0300

    Refactor collect_stats_from_users method

--- task-1.rb
+++ task-1.rb
@@ -26,11 +26,26 @@ def parse_session(fields)
   }
 end
 
-def collect_stats_from_users(report, users_objects, &block)
+def collect_stats_from_users(report, users_objects)
   users_objects.each do |user|
     user_key = "#{user[:first_name]} #{user[:last_name]}"
-    report[:usersStats][user_key] ||= {}
-    report[:usersStats][user_key] = report[:usersStats][user_key].merge(block.call(user))
+    browsers = user[:sessions].map { |s| s[:browser].upcase }.sort!
+    report[:usersStats][user_key] = {
+      # Собираем количество сессий по пользователям
+      sessionsCount: user[:sessions].count,
+      # Собираем количество времени по пользователям
+      totalTime: "#{user[:sessions].sum { |s| s[:time] }} min.",
+      # Выбираем самую длинную сессию пользователя
+      longestSession: "#{user[:sessions].max { |a, b| a[:time] <=> b[:time] }&.[](:time)} min.",
+      # Браузеры пользователя через запятую
+      browsers: browsers.join(', '),
+      # Хоть раз использовал IE?
+      usedIE: browsers.any? { |b| b =~ /INTERNET EXPLORER/i },
+      # Всегда использовал только Chrome?
+      alwaysUsedChrome: browsers.all? { |b| b =~ /CHROME/i },
+      # Даты сессий через запятую в обратном порядке в формате iso8601
+      dates: user[:sessions].map { |s| s[:date] }.sort.reverse!,
+    }
   end
 end
 
@@ -89,26 +104,7 @@ def work(filename, disable_gc: false)
   report[:allBrowsers] = unique_browsers.map(&:upcase).sort.join(',')
 
   # Статистика по пользователям
-
-  collect_stats_from_users(report, users) do |user|
-    browsers = user[:sessions].map { |s| s[:browser].upcase }.sort!
-    {
-      # Собираем количество сессий по пользователям
-      sessionsCount: user[:sessions].count,
-      # Собираем количество времени по пользователям
-      totalTime: "#{user[:sessions].sum { |s| s[:time] }} min.",
-      # Выбираем самую длинную сессию пользователя
-      longestSession: "#{user[:sessions].max { |a, b| a[:time] <=> b[:time] }&.[](:time)} min.",
-      # Браузеры пользователя через запятую
-      browsers: browsers.join(', '),
-      # Хоть раз использовал IE?
-      usedIE: browsers.any? { |b| b =~ /INTERNET EXPLORER/i },
-      # Всегда использовал только Chrome?
-      alwaysUsedChrome: browsers.all? { |b| b =~ /CHROME/i },
-      # Даты сессий через запятую в обратном порядке в формате iso8601
-      dates: user[:sessions].map { |s| s[:date] }.sort.reverse!,
-    }
-  end
+  collect_stats_from_users(report, users)
 
   File.write('result.json', "#{report.to_json}\n")
 end

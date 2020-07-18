9780240d21ef0b61eb15c27568c1b04a14a8ba74
Vlad Ortin <alpatre20@gmail.com>
2020-07-18 16:15:36 +0300

    Create time report

--- perform.rb
+++ perform.rb
@@ -3,12 +3,3 @@ require 'stackprof'
 require 'ruby-prof'
 
 require_relative 'task-1'
-
-p 'String process ...'
-
-GC.disable
-time = Benchmark.realtime do
-  work(file: 'data.txt')
-end
-
-p "Finish in #{time.round(2)}"
---
+++ run_time_report.rb
@@ -0,0 +1,20 @@
+require 'benchmark'
+require_relative 'task-1'
+GC.disable
+
+times = {}
+
+Dir['data_samples/*.txt'].each do |data_sample|
+  file_name = data_sample.split('/').last
+  p "String process: #{file_name}"
+  
+  time = Benchmark.realtime do
+    work(file: data_sample)
+  end
+
+  times[file_name] = time
+end
+
+File.open("reports/time_report_#{Time.now}.txt", 'w+') do |file|
+  times.each { |file_name, time| file.write("File: #{file_name} / Time: #{time.round(2)}\n") }
+end

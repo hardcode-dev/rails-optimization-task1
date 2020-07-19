244bc723c384cd58caa772928649f9186513be52
Vlad Ortin <alpatre20@gmail.com>
2020-07-19 15:41:23 +0300

    Revert work with parsing user / session

--- task_1.rb
+++ task_1.rb
@@ -15,20 +15,31 @@ class User
 end
 
 def parse_user(user)
-  fields = user.split(',').slice(1..-1)
-  Hash[%i[id first_name last_name age].zip(fields)]
+  fields = user.split(',')
+  {
+    id: fields[1],
+    first_name: fields[2],
+    last_name: fields[3],
+    age: fields[4]
+  }
 end
 
 def parse_session(session)
-  fields = session.split(',').slice(1..-1)
-  Hash[%i[user_id session_id browser time date].zip(fields)]
+  fields = session.split(',')
+  {
+    user_id: fields[1],
+    session_id: fields[2],
+    browser: fields[3],
+    time: fields[4],
+    date: fields[5]
+  }
 end
 
 def collect_stats_from_users(report, users_objects)
   users_objects.each do |user|
     user_key = "#{user.attributes[:first_name]} #{user.attributes[:last_name]}"
     report['usersStats'][user_key] ||= {}
-    report['usersStats'][user_key] = report['usersStats'][user_key].merge(yield(user))
+    report['usersStats'][user_key].merge!(yield(user))
   end
 end
 

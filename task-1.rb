7967d7553466a649c26d658df2b18325879f8fe1
Okatawa <krupnovbogdan@gmail.com>
2020-07-20 03:23:35 +0300

    Improve counting of unique browsers

--- task-1.rb
+++ task-1.rb
@@ -74,11 +74,7 @@ def work(path)
   report[:totalUsers] = users.count
 
   # Подсчёт количества уникальных браузеров
-  uniqueBrowsers = []
-  sessions.each do |session|
-    browser = session['browser']
-    uniqueBrowsers += [browser] if uniqueBrowsers.all? { |b| b != browser }
-  end
+  uniqueBrowsers = sessions.map { |session| session['browser'] }.uniq
 
   report['uniqueBrowsersCount'] = uniqueBrowsers.count
 

230a57ebae904364e8f70fdb27033030b1a03d5a
Ilya Lebedev <ilyalebedev1@yandex.ru>
2020-08-06 13:14:06 +0300

    feedback loop completed

--- benchmark.rb
+++ benchmark.rb
@@ -3,7 +3,7 @@
 require 'benchmark'
 require_relative 'task-1.rb'
 
-%i(5000 10000 25000 50000 100000).each do |size|
+%i(5000 10000 25000 50000 100000 large).each do |size|
   time = Benchmark.realtime do
     work("data_#{size}.txt")
   end

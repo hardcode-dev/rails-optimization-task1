8b6140ed4ce0414e87ed2fb54979506083a49272
Dmitry Fry <im@dmitryfry.com>
2020-09-19 21:05:15 +0300

    delete Date.parse

--- task-1.rb
+++ task-1.rb
@@ -137,7 +137,10 @@ def work
 
   # Даты сессий через запятую в обратном порядке в формате iso8601
   collect_stats_from_users(report, users_objects) do |user|
-    { 'dates' => user.sessions.map{|s| s['date']}.map {|d| Date.parse(d)}.sort.reverse.map { |d| d.iso8601 } }
+    { 'dates' => user.sessions.map do |s|
+      s['date']
+    end.sort.reverse
+    }
   end
 
   File.write('result.json', "#{report.to_json}\n")

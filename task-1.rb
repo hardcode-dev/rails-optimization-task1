0938ff90c937677c9cfe0de7b61920d9c7cde53d
denio-rus <denio@yandex.ru>
2021-04-21 22:16:57 +0300

    Optimization iteration 6
    Ipmrove Date parsing  (parse -> strptime)

--- task-1.rb
+++ task-1.rb
@@ -139,7 +139,7 @@ def work(file_name)
 
   # Даты сессий через запятую в обратном порядке в формате iso8601
   collect_stats_from_users(report, users_objects) do |user|
-    { 'dates' => user.sessions.map{|s| s['date']}.map {|d| Date.parse(d)}.sort.reverse.map { |d| d.iso8601 } }
+    { 'dates' => user.sessions.map{|s| s['date']}.map {|d| Date.strptime(d, '%Y-%m-%d')}.sort.reverse.map { |d| d.iso8601 } }
   end
 
   File.write('result.json', "#{report.to_json}\n")

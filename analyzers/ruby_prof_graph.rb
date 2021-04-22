4c35fca44e7597e07800aea3c7b761b41f035741
Konstantin Goryachev <kevinglare@gmail.com>
2021-04-22 16:31:06 +0300

    feat: adding ruby prof graph

--- analyzers/ruby_prof_graph.rb
+++ analyzers/ruby_prof_graph.rb
@@ -4,12 +4,12 @@ require 'ruby-prof'
 require_relative '../task_1'
 
 RubyProf.measure_mode = RubyProf::WALL_TIME
-DEMO_DATA = Dir['analyzers/demo_data/data_large.txt'].freeze
+DEMO_DATA = Dir['analyzers/demo_data/demo_data_30000.txt'].freeze
 
 DEMO_DATA.each do |data_path|
   result = RubyProf.profile do
     work(data_path)
   end
-  printer = RubyProf::FlatPrinter.new(result)
-  printer.print(File.open("analyzers/reports/ruby_prof_flat_#{data_path.split('_').last}", 'w+'))
+  printer = RubyProf::GraphHtmlPrinter.new(result)
+  printer.print(File.open("analyzers/reports/ruby_prof_graph_#{data_path.gsub(/[^\d]/, '')}.html", 'w+'))
 end

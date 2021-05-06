822f787fd9af25904f359596e05823d63075a3c5
avbelyshev <avbelishev@gmail.com>
2021-05-06 20:27:15 +0300

    Add profilers

---
+++ profilers/stackprof_cli.rb
@@ -0,0 +1,10 @@
+require 'stackprof'
+require_relative '../src/report'
+
+LINES_COUNT = 16_000
+
+GC.disable
+StackProf.run(mode: :wall, out: '../reports/stackprof.dump', interval: 1000) do
+  work('../data_large.txt', LINES_COUNT)
+end
+GC.enable
---
+++ profilers/stackprof_speedscope.rb
@@ -0,0 +1,13 @@
+require 'stackprof'
+require_relative '../src/report'
+
+LINES_COUNT = 16_000
+
+GC.disable
+profile = StackProf.run(mode: :wall, raw: true) do
+  work('../data_large.txt', LINES_COUNT)
+end
+
+File.write('../reports/stackprof.json', JSON.generate(profile))
+
+GC.enable

463d70e9efe16dbda45052824e98f6de906c7dee
ordm <ordm67@gmail.com>
2021-09-14 16:43:00 +0300

    spec refactoring

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -10,12 +10,17 @@ RSpec.describe 'Task №1' do
     let(:data) { File.read('spec/fixtures/data.txt') }
     let(:result_data) { File.read('spec/fixtures/result.json') }
 
-    before do
-      File.write('result.json', '')
-      File.write('data.txt', data)
-    end
+    context 'health check' do
+      before do
+        File.write('result.json', '')
+        File.write('data.txt', data)
+      end
 
-    context 'when the data size is small' do
+      after do
+        File.delete('result.json')
+        File.delete('data.txt')
+      end
+
       it 'returns users data(in json)' do
         work
         expect(File.read('result.json')).to eq(result_data)

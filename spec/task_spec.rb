a87cb580506b01edc9d7c24b42a78dd48c7db349
ordm <ordm67@gmail.com>
2021-09-14 16:30:23 +0300

    removed minitest

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,7 +1,25 @@
 # frozen_string_literal: true
 
 require 'rspec-benchmark'
+require_relative '../task-1'
 
 RSpec.describe 'Task №1' do
   include RSpec::Benchmark::Matchers
+
+  describe '#work' do
+    let(:data) { File.read('spec/fixtures/data.txt') }
+    let(:result_data) { File.read('spec/fixtures/result.json') }
+
+    before do
+      File.write('result.json', '')
+      File.write('data.txt', data)
+    end
+
+    context 'when the data size is small' do
+      it 'returns users data(in json)' do
+        work
+        expect(File.read('result.json')).to eq(result_data)
+      end
+    end
+  end
 end

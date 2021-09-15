dd7769776d3007f0ae72aa236fd6b38e6518ebc7
ordm <ordm67@gmail.com>
2021-09-15 15:34:07 +0300

    iteration №5

--- task-1.rb
+++ task-1.rb
@@ -14,9 +14,8 @@ class User
   end
 end
 
-def parse_user(user)
-  fields = user.split(',')
-  parsed_result = {
+def parse_user(fields)
+  {
     'id' => fields[1],
     'first_name' => fields[2],
     'last_name' => fields[3],
@@ -50,7 +49,7 @@ def work
 
   file_lines.each do |line|
     cols = line.split(',')
-    users = users + [parse_user(line)] if cols[0] == 'user'
+    users = users + [parse_user(cols)] if cols[0] == 'user'
     sessions = sessions + [parse_session(cols)] if cols[0] == 'session'
   end
 

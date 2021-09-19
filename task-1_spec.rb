4792aff5987b71a54bb5aa15644c67e369080569
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2021-09-19 16:17:45 +0300

    finish home work

--- task-1_spec.rb
+++ task-1_spec.rb
@@ -15,7 +15,7 @@ describe 'Perfomance with disable garbage collection' do
   end
 
   describe 'load large size file data_10000.txt with 1_000 lines and turn off garbage collection' do
-    it 'should works under 0.02 s' do
+    it 'should works under 5ms s' do
       expect {
           work('fixtures/data_1000.txt', disable_gc = false)
         }.to perform_under(5).ms.warmup(2).times.sample(10).times
@@ -23,7 +23,7 @@ describe 'Perfomance with disable garbage collection' do
   end
 
   describe 'load large size file data_10000.txt with 10_000 lines and turn off garbage collection' do
-    it 'should works under 0.8 s' do
+    it 'should works under 100ms s' do
       expect {
           work('fixtures/data_10000.txt', disable_gc = false)
         }.to perform_under(100).ms.warmup(2).times.sample(10).times
@@ -31,27 +31,18 @@ describe 'Perfomance with disable garbage collection' do
   end
 
   describe 'load large size file data_30000.txt with 20_000 lines and turn off garbage collection' do
-    it 'should works under 26,9 s' do
+    it 'should works under 6ms s' do
       expect {
           work('fixtures/data_30000.txt', disable_gc = false)
         }.to perform_under(600).ms.warmup(2).times.sample(10).times
     end
   end
 
-  # 
-  # describe 'load large size file data_100000.txt with 10_000 lines' do
-  #   it 'should works under 8 s' do
-  #       expect {
-  #           work('data_100000.txt')
-  #         }.to perform_under(2000).ms.warmup(2).times.sample(10).times
-  #   end 
-  # end
-
   describe 'load large size file data_large.txt' do
-    it 'should works under 35 s' do
+    it 'should works under 30s' do
       expect { 
         work('fixtures/data_large.txt', disable_gc = false)
-      }.to perform_under(35000).ms.warmup(2).times.sample(10).times
+      }.to perform_under(30000).ms.warmup(2).times.sample(10).times
     end
   end
 end
--- task-1_spec_without_gc.rb
+++ task-1_spec_without_gc.rb
@@ -15,43 +15,26 @@ describe 'Perfomance with disable garbage collection' do
   end
 
   describe 'load large size file data_10000.txt with 1_000 lines and turn off garbage collection' do
-    it 'should works under 0.02 s' do
+    it 'should works under 5ms' do
       expect {
           work('fixtures/data_1000.txt', disable_gc = true)
-        }.to perform_under(200).ms.warmup(2).times.sample(10).times
+        }.to perform_under(5).ms.warmup(2).times.sample(10).times
     end
   end
 
   describe 'load large size file data_10000.txt with 10_000 lines and turn off garbage collection' do
-    it 'should works under 0.8 s' do
+    it 'should works under 35ms' do
       expect {
           work('fixtures/data_10000.txt', disable_gc = true)
-        }.to perform_under(810).ms.warmup(2).times.sample(10).times
+        }.to perform_under(35).ms.warmup(2).times.sample(10).times
     end
   end
 
   describe 'load large size file data_30000.txt with 20_000 lines and turn off garbage collection' do
-    it 'should works under 26,9 s' do
+    it 'should works under 0.1ms' do
       expect {
           work('fixtures/data_30000.txt', disable_gc = true)
-        }.to perform_under(26900).ms.warmup(2).times.sample(10).times
+        }.to perform_under(100).ms.warmup(2).times.sample(10).times
     end
   end
-
-  # 
-  # describe 'load large size file data_100000.txt with 10_000 lines' do
-  #   it 'should works under 8 s' do
-  #       expect {
-  #           work('data_100000.txt')
-  #         }.to perform_under(2000).ms.warmup(2).times.sample(10).times
-  #   end 
-  # end
-
-  # describe 'load large size file data_large.txt' do
-  #   it 'should works under 30 ms' do
-  #     expect { 
-  #       work('fixtures/data_large.txt', disable_gc = false)
-  #     }.to perform_under(30).ms.warmup(2).times.sample(10).times
-  #   end
-  # end
 end

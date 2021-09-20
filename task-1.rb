873eeeeeb4acdb8d87b7baa6b52d9f5626620dc9
Kirill Usanov <kirill.usanov.dev@gmail.com>
2021-09-20 03:24:28 +0300

    Array#all? fix

--- task-1.rb
+++ task-1.rb
@@ -82,11 +82,7 @@ def work(filename = 'data_large.txt', disable_gc: false)
   report[:totalUsers] = users.count
 
   # Подсчёт количества уникальных браузеров
-  uniqueBrowsers = []
-  sessions.each do |session|
-    browser = session['browser']
-    uniqueBrowsers += [browser] if uniqueBrowsers.all? { |b| b != browser }
-  end
+  uniqueBrowsers = sessions.map { |s| s['browser'] }.uniq
 
   report['uniqueBrowsersCount'] = uniqueBrowsers.count
 

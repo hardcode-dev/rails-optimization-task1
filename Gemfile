798022cd935c8570015f28b376196d6b15f9a131
a.mukharryamov <a.mukharryamov@fun-box.ru>
2021-09-23 20:51:26 +0300

    prof reports

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem 'pry'
+gem 'ruby-prof'
+gem 'stackprof'
 
 group :test do
   gem 'rspec'
--- application.rb
+++ application.rb
@@ -1,3 +1,32 @@
 require_relative 'config/environment'
 
-GenerateReport.new.work('data.txt')
+# ---RUBYPROF START---
+RubyProf.measure_mode = RubyProf::WALL_TIME
+
+GC.disable
+
+result = RubyProf.profile do
+  GenerateReport.new.work('data.txt')
+end
+
+GC.enable
+
+File.open(
+  "/home/artur/Thinknetica/optimization/lesson-01/rails-optimization-task1/profiler_reports/ruby-prof_html_graph_report.html",
+  'w+'
+) do |file|
+  RubyProf::GraphHtmlPrinter.new(result).print(file)
+end
+# ---RUBYPROF END---
+
+# ---STACKPROF START---
+# StackProf.run(
+#   mode: :wall,
+#   out: '/home/artur/Thinknetica/optimization/lesson-01/rails-optimization-task1/profiler_reports/stack-prof_cli_report.dump',
+#   interval: 1000
+# ) do
+#   GC.disable
+#   GenerateReport.new.work('data.txt')
+#   GC.enable
+# end
+# ---STACKPROF END---

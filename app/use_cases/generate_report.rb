14bd5f53ba48a4c6cebc2bb6c3bad3756b777620
a.mukharryamov <a.mukharryamov@fun-box.ru>
2021-09-26 06:55:38 +0300

    optimised: method #select

--- app/use_cases/generate_report.rb
+++ app/use_cases/generate_report.rb
@@ -92,9 +92,11 @@ class GenerateReport
     # Статистика по пользователям
     users_objects = []
 
+    sessions_by_user = @sessions.group_by { |s| s['user_id']}
+
     @users.each do |user|
       attributes = user
-      user_sessions = @sessions.select { |session| session['user_id'] == user['id'] }
+      user_sessions = sessions_by_user[user['id']]
       user_object = User.new(attributes: attributes, sessions: user_sessions)
       users_objects = users_objects + [user_object]
     end

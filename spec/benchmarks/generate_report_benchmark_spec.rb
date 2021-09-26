9b2233a6c737c792b1d6eb59348383399e5fbe3d
a.mukharryamov <a.mukharryamov@fun-box.ru>
2021-09-26 06:58:00 +0300

    test for 16000 lines

--- spec/benchmarks/generate_report_benchmark_spec.rb
+++ spec/benchmarks/generate_report_benchmark_spec.rb
@@ -1,9 +1,19 @@
 RSpec.describe GenerateReport do
-  let(:path) { 'spec/support/fixtures/data_large.txt' }
+  # let(:path) { 'spec/support/fixtures/data_large.txt' }
 
-  before { File.write('result.json', '') }
+  # before { File.write('result.json', '') }
 
-  it 'work time is less than 30 seconds' do
-    expect { subject.work(path) }.to perform_under(30).sec.warmup(2).sample(5)
+  # it 'work time is less than 30 seconds' do
+  #   expect { subject.work(path) }.to perform_under(30).sec.warmup(2).sample(5)
+  # end
+
+  context 'data_16000' do
+    let(:path) { 'spec/support/fixtures/data_16000.txt' }
+
+    before { File.write('result.json', '') }
+
+    it 'work time is less than 1 seconds' do
+      expect { subject.work(path) }.to perform_under(30).sec.warmup(2).sample(5)
+    end
   end
 end

b04d39e25e619ecc55e68f3816cb631d01c2dbf6
Dmytro Angelovskyi <dmitriy.angelovskiy@srgh.tech>
2021-09-28 01:18:15 +0300

    [ADD] Preparings for refactor

---
+++ profiler.rb
@@ -0,0 +1,10 @@
+require 'ruby-prof'
+require_relative './task-1'
+
+RubyProf.measure_mode = RubyProf::WALL_TIME
+
+results = RubyProf.profile do
+  work('data50000.txt', disable_gc: true)
+end
+
+RubyProf::MultiPrinter.new(results, %i[flat graph graph_html tree call_info stack dot]).print(path: './reports', profile: 'profile')

31169baa253ed6ab288dc7a59ec9de1914cd8d21
alex707 <alex.grigorenko2942@gmail.com>
2021-09-28 17:44:12 +0300

    prepare script for optimization

--- task-1.rb
+++ task-1.rb
@@ -3,7 +3,11 @@
 require 'json'
 require 'pry'
 require 'date'
-require 'minitest/autorun'
+require 'minitest'
+require 'minitest/autorun' if ENV['RACK_ENV'] == 'test'
+require 'minitest/benchmark' if ENV['RACK_ENV'] == 'test'
+require 'ruby-prof' if ENV['RACK_ENV'] == 'benchmark'
+require 'stackprof' if ENV['RACK_ENV'] == 'benchmark'
 
 class User
   attr_reader :attributes, :sessions
@@ -44,11 +48,28 @@ def collect_stats_from_users(report, users_objects, &block)
 end
 
 def work
-  file_lines = File.read('data.txt').split("\n")
+  t0 = Time.now
+  file_name = 'data_large.txt'
+  file_name = 'data.txt' if ENV['RACK_ENV'] == 'test'
+  file_lines = File.read(file_name).split("\n")
 
   users = []
   sessions = []
 
+  # user_record:
+  #   - 0 - type
+  #   - 1 - id
+  #   - 2 - first_name
+  #   - 3 - last_name
+  #   - 4 - age
+
+  # session_record:
+  #   - 0 - type
+  #   - 1 - user_id
+  #   - 2 - session_id
+  #   - 3 - browser
+  #   - 4 - time
+  #   - 5 - date
   file_lines.each do |line|
     cols = line.split(',')
     users = users + [parse_user(line)] if cols[0] == 'user'
@@ -141,12 +162,15 @@ def work
   end
 
   File.write('result.json', "#{report.to_json}\n")
+
+  puts "total: #{Time.now - t0}"
 end
 
-class TestMe < Minitest::Test
-  def setup
-    File.write('result.json', '')
-    File.write('data.txt',
+if ENV['RACK_ENV'] == 'test'
+  class TestMe < Minitest::Test
+    def setup
+      File.write('result.json', '')
+      File.write('data.txt',
 'user,0,Leida,Cira,0
 session,0,0,Safari 29,87,2016-10-23
 session,0,1,Firefox 12,118,2017-02-27
@@ -166,11 +190,38 @@ session,2,1,Safari 49,85,2017-05-22
 session,2,2,Firefox 47,17,2018-02-02
 session,2,3,Chrome 20,84,2016-11-25
 ')
+    end
+
+    def test_result
+      work
+      expected_result = '{"totalUsers":3,"uniqueBrowsersCount":14,"totalSessions":15,"allBrowsers":"CHROME 13,CHROME 20,CHROME 35,CHROME 6,FIREFOX 12,FIREFOX 32,FIREFOX 47,INTERNET EXPLORER 10,INTERNET EXPLORER 28,INTERNET EXPLORER 35,SAFARI 17,SAFARI 29,SAFARI 39,SAFARI 49","usersStats":{"Leida Cira":{"sessionsCount":6,"totalTime":"455 min.","longestSession":"118 min.","browsers":"FIREFOX 12, INTERNET EXPLORER 28, INTERNET EXPLORER 28, INTERNET EXPLORER 35, SAFARI 29, SAFARI 39","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-09-27","2017-03-28","2017-02-27","2016-10-23","2016-09-15","2016-09-01"]},"Palmer Katrina":{"sessionsCount":5,"totalTime":"218 min.","longestSession":"116 min.","browsers":"CHROME 13, CHROME 6, FIREFOX 32, INTERNET EXPLORER 10, SAFARI 17","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-04-29","2016-12-28","2016-12-20","2016-11-11","2016-10-21"]},"Gregory Santos":{"sessionsCount":4,"totalTime":"192 min.","longestSession":"85 min.","browsers":"CHROME 20, CHROME 35, FIREFOX 47, SAFARI 49","usedIE":false,"alwaysUsedChrome":false,"dates":["2018-09-21","2018-02-02","2017-05-22","2016-11-25"]}}}' + "\n"
+      assert_equal expected_result, File.read('result.json')
+    end
+  end
+elsif ENV['RACK_ENV'] == 'benchmark'
+  result = RubyProf.profile do
+    work
+  end
+  printer = RubyProf::FlatPrinter.new(result)
+  printer.print(File.open("ruby_prof_reports/flat.txt", "w+"))
+
+  printer = RubyProf::GraphHtmlPrinter.new(result)
+  printer.print(File.open("ruby_prof_reports/graph.html", "w+"))
+
+  printer = RubyProf::CallStackPrinter.new(result)
+  printer.print(File.open('ruby_prof_reports/callstack.html', 'w+'))
+
+  printer = RubyProf::CallTreePrinter.new(result)
+  printer.print(:path => "ruby_prof_reports", :profile => 'callgrind')
+
+  StackProf.run(mode: :wall, out: 'stackprof_reports/stackprof.dump', interval: 1000) do
+    work
   end
 
-  def test_result
+  profile = StackProf.run(mode: :wall, raw: true) do
     work
-    expected_result = '{"totalUsers":3,"uniqueBrowsersCount":14,"totalSessions":15,"allBrowsers":"CHROME 13,CHROME 20,CHROME 35,CHROME 6,FIREFOX 12,FIREFOX 32,FIREFOX 47,INTERNET EXPLORER 10,INTERNET EXPLORER 28,INTERNET EXPLORER 35,SAFARI 17,SAFARI 29,SAFARI 39,SAFARI 49","usersStats":{"Leida Cira":{"sessionsCount":6,"totalTime":"455 min.","longestSession":"118 min.","browsers":"FIREFOX 12, INTERNET EXPLORER 28, INTERNET EXPLORER 28, INTERNET EXPLORER 35, SAFARI 29, SAFARI 39","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-09-27","2017-03-28","2017-02-27","2016-10-23","2016-09-15","2016-09-01"]},"Palmer Katrina":{"sessionsCount":5,"totalTime":"218 min.","longestSession":"116 min.","browsers":"CHROME 13, CHROME 6, FIREFOX 32, INTERNET EXPLORER 10, SAFARI 17","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-04-29","2016-12-28","2016-12-20","2016-11-11","2016-10-21"]},"Gregory Santos":{"sessionsCount":4,"totalTime":"192 min.","longestSession":"85 min.","browsers":"CHROME 20, CHROME 35, FIREFOX 47, SAFARI 49","usedIE":false,"alwaysUsedChrome":false,"dates":["2018-09-21","2018-02-02","2017-05-22","2016-11-25"]}}}' + "\n"
-    assert_equal expected_result, File.read('result.json')
   end
+  File.write('stackprof_reports/stackprof.json', JSON.generate(profile))
+else
+  work
 end

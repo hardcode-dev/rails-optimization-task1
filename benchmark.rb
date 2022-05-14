1dea908d1f3fe0cc16e39863eec999555e933fff
farid <farid.babazov@gmail.com>
2022-05-14 22:58:40 +0600

    add benchmark info without disable GC

--- benchmark.rb
+++ benchmark.rb
@@ -2,7 +2,7 @@ require 'benchmark'
 require_relative './task-1'
 
 time = Benchmark.realtime do |x|
-  work(file_name: 'fixtures/data_large.txt', disable_gc: true)
+  work(file_name: 'fixtures/data_large.txt', disable_gc: false)
 end
 
 puts time

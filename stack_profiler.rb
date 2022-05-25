e6cc9cef08d64514d413cd650ade392548a3af8c
dmitriy-romanenko <romanenko.dmitriy.97@gmail.com>
2022-05-25 09:29:12 +0400

    delete unnecessary files

--- stack_profiler.rb
+++ stack_profiler.rb
@@ -1,8 +1,6 @@
 require 'stackprof'
 require_relative 'work_method.rb'
 
-# RubyProf.measure_mode = RubyProf::WALL_TIME
-
 ROWS_COUNT = 100000
 
 StackProf.run(mode: :wall, out: "stackprof_reports/stackprof.dump", interval: 1000) do

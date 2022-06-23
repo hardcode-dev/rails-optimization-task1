1df6745469771f5a8ec25302dc286d2fa15f186e
Evgeniy Stashkovyan <stashevgenij@yandex.ru>
2022-06-23 08:07:25 +0300

    finish

--- task-1.rb
+++ task-1.rb
@@ -19,22 +19,21 @@ class User
 end
 
 def parse_user(fields)
-  parsed_result = {
+  User.new(attributes: {
     'id' => fields[1],
     'first_name' => fields[2],
     'last_name' => fields[3],
     'age' => fields[4],
     'sessions' => [],
-  }
-  User.new(attributes: parsed_result, sessions: [])
+  }, sessions: [])
 end
 
 def parse_session(fields)
   {
     'user_id' => fields[1],
     'session_id' => fields[2],
-    'browser' => fields[3],
-    'time' => fields[4],
+    'browser' => fields[3].upcase,
+    'time' => fields[4].to_i,
     'date' => fields[5],
   }
 end
@@ -46,10 +45,11 @@ def collect_stats_from_users(report, users_objects, &block)
   end
 end
 
-def work(file = 'data_large.txt', disable_gc = false)
+def work(file = 'data_large.txt', disable_gc = false, lines = nil)
   GC.disable if disable_gc
 
   file_lines = File.read(file).split("\n")
+  file_lines = file_lines.first(lines) if lines
 
   # progressbar = ProgressBar.create(
   #   total: file_lines.size,
@@ -62,12 +62,13 @@ def work(file = 'data_large.txt', disable_gc = false)
 
   file_lines.each do |line|
     cols = line.split(',')
+    user_id = cols[1].to_i
 
-    users_objects[cols[1].to_i] = parse_user(cols) if cols[0] == 'user'
+    users_objects[user_id] = parse_user(cols) if cols[0] == 'user'
 
     if cols[0] == 'session'
       session = parse_session(cols) 
-      users_objects[cols[1].to_i].sessions << session
+      users_objects[user_id].sessions << session
       sessions << session
     end
 
@@ -93,7 +94,7 @@ def work(file = 'data_large.txt', disable_gc = false)
 
   report[:totalUsers] = users_objects.count
 
-  uniq_browsers = sessions.map { |s| s['browser'].upcase }.uniq
+  uniq_browsers = sessions.map { |s| s['browser'] }.uniq
 
   report['uniqueBrowsersCount'] = uniq_browsers.count
 
@@ -109,8 +110,8 @@ def work(file = 'data_large.txt', disable_gc = false)
     dates = []
 
     user.sessions.each do |s|
-      sessions_time << s['time'].to_i
-      browsers << s['browser'].upcase
+      sessions_time << s['time']
+      browsers << s['browser']
       dates << s['date']
     end
 
@@ -142,35 +143,3 @@ def work(file = 'data_large.txt', disable_gc = false)
 
   File.write('result.json', "#{Oj.dump(report, mode: :strict)}\n")
 end
-
-# class TestMe < Minitest::Test
-#   def setup
-#     File.write('result.json', '')
-#     File.write('data.txt',
-# 'user,0,Leida,Cira,0
-# session,0,0,Safari 29,87,2016-10-23
-# session,0,1,Firefox 12,118,2017-02-27
-# session,0,2,Internet Explorer 28,31,2017-03-28
-# session,0,3,Internet Explorer 28,109,2016-09-15
-# session,0,4,Safari 39,104,2017-09-27
-# session,0,5,Internet Explorer 35,6,2016-09-01
-# user,1,Palmer,Katrina,65
-# session,1,0,Safari 17,12,2016-10-21
-# session,1,1,Firefox 32,3,2016-12-20
-# session,1,2,Chrome 6,59,2016-11-11
-# session,1,3,Internet Explorer 10,28,2017-04-29
-# session,1,4,Chrome 13,116,2016-12-28
-# user,2,Gregory,Santos,86
-# session,2,0,Chrome 35,6,2018-09-21
-# session,2,1,Safari 49,85,2017-05-22
-# session,2,2,Firefox 47,17,2018-02-02
-# session,2,3,Chrome 20,84,2016-11-25
-# ')
-#   end
-
-#   def test_result
-#     work
-#     expected_result = '{"totalUsers":3,"uniqueBrowsersCount":14,"totalSessions":15,"allBrowsers":"CHROME 13,CHROME 20,CHROME 35,CHROME 6,FIREFOX 12,FIREFOX 32,FIREFOX 47,INTERNET EXPLORER 10,INTERNET EXPLORER 28,INTERNET EXPLORER 35,SAFARI 17,SAFARI 29,SAFARI 39,SAFARI 49","usersStats":{"Leida Cira":{"sessionsCount":6,"totalTime":"455 min.","longestSession":"118 min.","browsers":"FIREFOX 12, INTERNET EXPLORER 28, INTERNET EXPLORER 28, INTERNET EXPLORER 35, SAFARI 29, SAFARI 39","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-09-27","2017-03-28","2017-02-27","2016-10-23","2016-09-15","2016-09-01"]},"Palmer Katrina":{"sessionsCount":5,"totalTime":"218 min.","longestSession":"116 min.","browsers":"CHROME 13, CHROME 6, FIREFOX 32, INTERNET EXPLORER 10, SAFARI 17","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-04-29","2016-12-28","2016-12-20","2016-11-11","2016-10-21"]},"Gregory Santos":{"sessionsCount":4,"totalTime":"192 min.","longestSession":"85 min.","browsers":"CHROME 20, CHROME 35, FIREFOX 47, SAFARI 49","usedIE":false,"alwaysUsedChrome":false,"dates":["2018-09-21","2018-02-02","2017-05-22","2016-11-25"]}}}' + "\n"
-#     assert_equal expected_result, File.read('result.json')
-#   end
-# end

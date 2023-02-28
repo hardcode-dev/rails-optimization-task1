e87d813c262527e0bc23e763bc8cb8f02c6dd4a0
Tab Loid <tabloidmeister@gmail.com>
2023-03-01 01:47:42 +0300

    Avoids unnecessary pass through the array

--- task-1.rb
+++ task-1.rb
@@ -95,9 +95,11 @@ def work(file_name = 'data.txt')
   # Статистика по пользователям
   users_objects = []
 
+  grouped_sessions = sessions.group_by { |s| s['user_id'] }
+
   users.each do |user|
     attributes = user
-    user_sessions = sessions.select { |session| session['user_id'] == user['id'] }
+    user_sessions = grouped_sessions[user['id']]
     user_object = User.new(attributes: attributes, sessions: user_sessions)
     users_objects = users_objects + [user_object]
   end

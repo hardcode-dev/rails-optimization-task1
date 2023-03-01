cd4f08029b902934959315900da0125bf29cdb61
Tab Loid <tabloidmeister@gmail.com>
2023-03-01 13:42:13 +0300

    Avoids unnecessary allocations for arrays

--- task-1.rb
+++ task-1.rb
@@ -50,8 +50,8 @@ def work(file_name = 'data.txt')
 
   file_lines.each do |line|
     cols = line.split(',')
-    users = users + [parse_user(line)] if cols[0] == 'user'
-    sessions = sessions + [parse_session(line)] if cols[0] == 'session'
+    users << parse_user(line) if cols[0] == 'user'
+    sessions << parse_session(line) if cols[0] == 'session'
   end
 
   # Отчёт в json
@@ -77,7 +77,7 @@ def work(file_name = 'data.txt')
   uniqueBrowsers = []
   sessions.each do |session|
     browser = session['browser']
-    uniqueBrowsers += [browser] if uniqueBrowsers.all? { |b| b != browser }
+    uniqueBrowsers << browser if uniqueBrowsers.all? { |b| b != browser }
   end
 
   report['uniqueBrowsersCount'] = uniqueBrowsers.count
@@ -101,7 +101,7 @@ def work(file_name = 'data.txt')
     attributes = user
     user_sessions = grouped_sessions[user['id']]
     user_object = User.new(attributes: attributes, sessions: user_sessions)
-    users_objects = users_objects + [user_object]
+    users_objects << user_object
   end
 
   report['usersStats'] = {}

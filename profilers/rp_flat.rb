0f115a13683b5bf0212c28464c2a6b44119b7e94
Арапов Александр <aarapov@at-consulting.ru>
2023-03-04 16:47:40 +0300

    Добавление аргумента в метод work

--- profilers/rp_flat.rb
+++ profilers/rp_flat.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 require 'ruby-prof'
-require_relative '../task-1'
+require_relative '../task_1'
 
 RubyProf.measure_mode = RubyProf::WALL_TIME
 
 result = RubyProf.profile do
-  work
+  work('data10000.txt')
 end
 
 printer = RubyProf::FlatPrinter.new(result)

4e8af575ca3371eeefee89d0cf0f15805c72dc27
Nikita Plyusnin <nplyusnin78@gmail.com>
2023-03-08 00:37:36 +0300

    add result text to study template

--- lib/user.rb
+++ lib/user.rb
@@ -4,20 +4,20 @@ require 'date'
 
 def parse_user(fields)
   {
-    'id' => fields[1],
-    'first_name' => fields[2],
-    'last_name' => fields[3],
-    'age' => fields[4],
+    id: fields[1],
+    first_name: fields[2],
+    last_name: fields[3],
+    age: fields[4],
   }
 end
 
 def parse_session(fields)
   {
-    'user_id' => fields[1],
-    'session_id' => fields[2],
-    'browser' => fields[3].upcase,
-    'time' => fields[4],
-    'date' => fields[5],
+    user_id: fields[1],
+    session_id: fields[2],
+    browser: fields[3].upcase,
+    time: fields[4],
+    date: fields[5],
   }
 end
 
@@ -30,11 +30,11 @@ def work(input_file)
 
     if cols[0] == 'user'
       user = parse_user(cols)
-      users[user['id']] = user
+      users[user[:id]] = user
     else
       session = parse_session(cols)
-      sessions[session['user_id']] ||= {}
-      sessions[session['user_id']][session['session_id']] = session
+      sessions[session[:user_id]] ||= {}
+      sessions[session[:user_id]][session[:session_id]] = session
     end
   end
 
@@ -61,8 +61,8 @@ def work(input_file)
   uniqueBrowsers = {}
   sessions.each do |user_id, user_sessions|
     user_sessions.each do |session_id, session|
-      browser = session['browser']
-      uniqueBrowsers[session['browser']] = nil
+      browser = session[:browser]
+      uniqueBrowsers[session[:browser]] = nil
     end
   end
   uniqueBrowsers = uniqueBrowsers.keys
@@ -72,7 +72,7 @@ def work(input_file)
 
   all_browsers = {}
   sessions.each do |user_id, user_sessions|
-    user_sessions.each { |_k, user_session| all_browsers[user_session['browser']] = nil }
+    user_sessions.each { |_k, user_session| all_browsers[user_session[:browser]] = nil }
   end
   
   report['allBrowsers'] = all_browsers.keys.sort.join(',')
@@ -81,16 +81,16 @@ def work(input_file)
 
   sessions.each do |user_id, user_sessions|
     user = users[user_id]
-    user_key = "#{user['first_name']}" + ' ' + "#{user['last_name']}"
+    user_key = "#{user[:first_name]}" + ' ' + "#{user[:last_name]}"
 
     report['usersStats'][user_key] = { 
       'sessionsCount' => user_sessions.count,
-      'totalTime' => user_sessions.map { |_, s| s['time'].to_i}.sum.to_s + ' min.',
-      'longestSession' => user_sessions.map {|_, s| s['time'].to_i}.max.to_s + ' min.',
-      'browsers' => user_sessions.map {|_, s| s['browser']}.sort.join(', '),
-      'usedIE' => user_sessions.any?{|_, s| s['browser'].start_with?("INTERNET EXPLORER")},
-      'alwaysUsedChrome' => user_sessions.all?{|_, s| s['browser'].start_with?("CHROME")},
-      'dates' => user_sessions.map{|_, s| Date.strptime(s['date'], '%Y-%m-%d')}.sort.reverse.map { |d| d.iso8601 }
+      'totalTime' => user_sessions.map { |_, s| s[:time].to_i}.sum.to_s + ' min.',
+      'longestSession' => user_sessions.map {|_, s| s[:time].to_i}.max.to_s + ' min.',
+      'browsers' => user_sessions.map {|_, s| s[:browser]}.sort.join(', '),
+      'usedIE' => user_sessions.any?{|_, s| s[:browser].start_with?("INTERNET EXPLORER")},
+      'alwaysUsedChrome' => user_sessions.all?{|_, s| s[:browser].start_with?("CHROME")},
+      'dates' => user_sessions.map{|_, s| Date.strptime(s[:date], '%Y-%m-%d')}.sort.reverse.map { |d| d.iso8601 }
     }
   end
 

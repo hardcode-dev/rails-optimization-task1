a81da43879b110ef1a89c79987ce177cadc8f624
Pavel Biarkovich <pavel.biarkovich@softswiss.com>
2023-03-07 04:34:29 +0300

    Add benchmark testing

--- scripts/generate_data.rb
+++ scripts/generate_data.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-AVAILABLE_FILE_SIZES = [1_000, 2_000, 4_000, 8_000, 16_000, 32_000].freeze
+AVAILABLE_FILE_SIZES = [1_000, 2_000, 4_000, 8_000, 16_000, 32_000, 64_000, 128_000].freeze
 
 AVAILABLE_FILE_SIZES.each do |line|
   `head -n #{line} data/data_large.txt > data/data-#{line}-lines.txt`

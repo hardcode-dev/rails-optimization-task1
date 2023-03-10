6b9f5bf25a04bd0baead7a3b034e3b78875044a0
Nikita <trukhanov@tiqum.ru>
2023-03-10 13:46:55 +0500

    final

--- spec/benchmark_spec.rb
+++ spec/benchmark_spec.rb
@@ -31,7 +31,7 @@ describe 'Data' do
     ]
   end
   let(:result) { '{"totalUsers":3,"uniqueBrowsersCount":14,"totalSessions":15,"allBrowsers":"CHROME 13,CHROME 20,CHROME 35,CHROME 6,FIREFOX 12,FIREFOX 32,FIREFOX 47,INTERNET EXPLORER 10,INTERNET EXPLORER 28,INTERNET EXPLORER 35,SAFARI 17,SAFARI 29,SAFARI 39,SAFARI 49","usersStats":{"Leida Cira":{"sessionsCount":6,"totalTime":"455 min.","longestSession":"118 min.","browsers":"FIREFOX 12, INTERNET EXPLORER 28, INTERNET EXPLORER 28, INTERNET EXPLORER 35, SAFARI 29, SAFARI 39","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-09-27","2017-03-28","2017-02-27","2016-10-23","2016-09-15","2016-09-01"]},"Palmer Katrina":{"sessionsCount":5,"totalTime":"218 min.","longestSession":"116 min.","browsers":"CHROME 13, CHROME 6, FIREFOX 32, INTERNET EXPLORER 10, SAFARI 17","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-04-29","2016-12-28","2016-12-20","2016-11-11","2016-10-21"]},"Gregory Santos":{"sessionsCount":4,"totalTime":"192 min.","longestSession":"85 min.","browsers":"CHROME 20, CHROME 35, FIREFOX 47, SAFARI 49","usedIE":false,"alwaysUsedChrome":false,"dates":["2018-09-21","2018-02-02","2017-05-22","2016-11-25"]}}}' + "\n"  }
-  it 'ddd' do
+  it 'is equal' do
     work(file_lines: data)
     expect(result).to eq(File.read('result.json'))
   end
@@ -41,13 +41,25 @@ describe 'Performance' do
   describe 'with 10000 rows' do
     let(:file_lines) { File.read('data_large.txt').split("\n").first(10_000) }
 
-    it 'works under 10 ms' do
-      expect { work(file_lines: file_lines) }.to perform_under(10).ms.warmup(2).times.sample(5).times
+    it 'works under 40 ms' do
+      expect { work(file_lines: file_lines) }.to perform_under(40).ms.warmup(2).times.sample(5).times
     end
+  end
 
-    # it 'performs linear' do
-    #   expect { |n, _i| linear_work(n) }.to perform_linear.in_range(10, 10_000)
-    # end
+  describe 'with 20000 rows' do
+    let(:file_lines) { File.read('data_large.txt').split("\n").first(20_000) }
+
+    it 'works under 80 ms' do
+      expect { work(file_lines: file_lines) }.to perform_under(80).ms.warmup(2).times.sample(5).times
+    end
+  end
+
+  describe 'with 30000 rows' do
+    let(:file_lines) { File.read('data_large.txt').split("\n").first(30_000) }
+
+    it 'works under 120 ms' do
+      expect { work(file_lines: file_lines) }.to perform_under(120).ms.warmup(2).times.sample(5).times
+    end
   end
 end
 

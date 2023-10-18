f062fd4b514b400451be6d48d483870e243855b8
Udalov Igor <i.udalov@rambler-co.ru>
2023-10-18 10:00:06 +0300

    Increase check size x 5 (10k -> 50k) and metric (245ms -> 900 ms)

--- spec/work/performance/speed_spec.rb
+++ spec/work/performance/speed_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe 'Work' do
 
     let(:file_path) { fixture(size) }
     let(:warmup_seconds) { 1 }
-    let(:size) { 10_000 }
-    let(:ms) { 245 }
+    let(:size) { 50_000 }
+    let(:ms) { 900 }
 
     before { ensure_test_data_exists(size) }
 

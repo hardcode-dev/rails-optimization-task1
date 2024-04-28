0880b61e82d214b1f9416a7c112af5eb0918b29e
Alex Yaroslavzev <yaroslavzevalex@gmail.com>
2024-04-28 15:14:04 +0200

    add case study result

--- task-1.rb
+++ task-1.rb
@@ -6,10 +6,18 @@ require 'json'
 require 'pry'
 require 'date'
 
-GC.disable
+
 
 require_relative 'report/report'
 require_relative 'report/parser'
+
+require_relative 'report/v1/report'
+require_relative 'report/v1/parser'
+
+require_relative 'report/v2/report'
+require_relative 'report/v2/parser'
+
+GC.disable
 
 class InitWork
   def self.work(file_name = 'data.txt')
@@ -17,6 +25,28 @@ class InitWork
 
     users, sessions = parse(file_lines)
     report = report(users, sessions)
+
+    File.write('result.json', "#{report.to_json}\n")
+  end
+end
+
+class WorkV1
+  def self.work(file_name = 'data.txt')
+    file_lines = File.read(file_name).split("\n")
+
+    users, sessions, sessions_hash = V1.parse(file_lines)
+    report = V1.report(users, sessions, sessions_hash)
+
+    File.write('result.json', "#{report.to_json}\n")
+  end
+end
+
+class WorkV2
+  def self.work(file_name = 'data.txt')
+    file_lines = File.read(file_name).split("\n")
+
+    users, sessions, sessions_hash = V2.parse(file_lines)
+    report = V2.report(users, sessions, sessions_hash)
 
     File.write('result.json', "#{report.to_json}\n")
   end

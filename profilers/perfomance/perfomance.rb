e328c7381c31fd31066cdc4ebd69cf164dda1700
Alex Yaroslavzev <yaroslavzevalex@gmail.com>
2024-04-28 14:55:43 +0200

    use hash instead of array

--- profilers/perfomance/perfomance.rb
+++ profilers/perfomance/perfomance.rb
@@ -2,6 +2,7 @@
 
 require 'rspec-benchmark'
 require_relative '../../task-1'
+require_relative '../../task-1-v-1'
 
 describe 'Task-1' do
   include RSpec::Benchmark::Matchers
@@ -13,12 +14,34 @@ describe 'Task-1' do
   describe 'Performance' do
     let(:file_name) { './data4000.txt' }
 
-    it 'works under 6 ms' do
+    it 'works under 550 ms' do
       expect do
-        work(file_name)
+        InitWork.work(file_name)
       end.to perform_under(550).ms.warmup(2).times.sample(10).times
     end
   end
+
+  describe 'Performance V1' do
+    let(:file_name) { './data4000.txt' }
+
+    it 'works under 100 ms' do
+      expect do
+        WorkV1.work(file_name)
+      end.to perform_under(100).ms.warmup(2).times.sample(10).times
+    end
+
+    it 'works under 100 ms' do
+      expect { WorkV1.work(file_name) }.to perform_faster_than { InitWork.work(file_name) }.at_least(4).times
+    end
+  end
+
+  # describe 'Performance' do
+  #   let(:file_name) { './data4000.txt' }
+
+  #   it 'works under 550 ms' do
+  #   expect { work_v1(file_name) }.to perform_faster_than { work(file_name) }.twice
+  #   end
+  # end
 
   #   describe 'Complexity' do
   #     let (:file_names) { ['data1000.txt', 'data2000.txt', 'data3000.txt', 'data4000.txt'] }
--- task-1-v-1.rb
+++ task-1-v-1.rb
@@ -6,17 +6,17 @@ require 'json'
 require 'pry'
 require 'date'
 
+require_relative 'report/v1/report'
+require_relative 'report/v1/parser'
+
 GC.disable
 
-require_relative 'report/report'
-require_relative 'report/parser'
-
-class InitWork
+class WorkV1
   def self.work(file_name = 'data.txt')
     file_lines = File.read(file_name).split("\n")
 
-    users, sessions = parse(file_lines)
-    report = report(users, sessions)
+    users, sessions, sessions_hash = V1.parse(file_lines)
+    report = V1.report(users, sessions, sessions_hash)
 
     File.write('result.json', "#{report.to_json}\n")
   end

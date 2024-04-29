e89db7ba71c0803cffe23e46643e2a32506b09c3
potashin <potashin.nikita@gmail.com>
2024-04-29 16:16:37 +0300

    chore: adjust specs

--- spec/task-1_spec.rb
+++ spec/task-1_spec.rb
@@ -2,20 +2,26 @@ require 'rspec'
 require 'rspec-benchmark'
 require_relative '../task-1'
 
-RSpec.configure do |config|
-  config.include RSpec::Benchmark::Matchers
-end
-
 RSpec.describe 'work' do
+  include RSpec::Benchmark::Matchers
+
   it 'should be linear' do
-    `head -n 10000 data_large.txt > data.txt`
+    expect { |number, _|
+      `head -n #{number * 1000} data_large.txt > data.txt`
 
-    expect { work }.to perform_linear
+      work
+    }.to perform_linear.in_range(1, 100)
   end
 
-  it 'should perform under 15 seconds' do
-    `cp data_large.txt data.txt`
+  it 'should perform under 5 seconds' do
+    `head -n 1000000 data_large.txt > data.txt`
 
-    expect { work }.to perform_under(30).sec
+    expect { work }.to perform_under(5).sec
   end
+
+  # it 'should perform under 30 seconds' do
+  #   `cp data_large.txt data.txt`
+
+  #   expect { work }.to perform_under(30).sec
+  # end
 end

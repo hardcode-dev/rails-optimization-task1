df145839c8f8c73f80a7a88a178dd6ed543b40d9
Nikita Kononov <vocrsz@gmail.com>
2024-04-29 19:32:24 +0200

    implement complexity testing

--- spec/performance_spec.rb
+++ spec/performance_spec.rb
@@ -1,21 +1,42 @@
 require_relative '../task-1.rb'
 
 describe "Perfomance" do
-  it "works under 3ms for 100 strings of data" do
-    expect {
-      work('spec/fixtures/sample100.txt')
-    }.to perform_under(3).ms.warmup(2).times.sample(10).times
+
+  context "works under 3ms for 100 strings of data" do
+    let(:size) { 100 }
+
+    it {
+      prepare_data(size) do |filename|
+        expect {
+          work(filename)
+        }.to perform_under(3).ms.warmup(2).times.sample(10).times
+      end
+    }
   end
 
-  it "works under 0.05s for 1000 strings of data" do
-    expect {
-      work('spec/fixtures/sample1000.txt')
-    }.to perform_under(50).ms.warmup(2).times.sample(10).times
+  context "works under 0.05s for 1000 strings of data" do
+    let(:size) { 1000 }
+
+    it {
+      prepare_data(size) do |filename|
+        expect {
+          work(filename)
+        }.to perform_under(50).ms.warmup(2).times.sample(10).times
+      end
+    }
   end
 
   xit "works under 4s for 10000 strings of data" do
     expect {
       work('spec/fixtures/sample10000.txt')
     }.to perform_under(4).sec.warmup(2).times.sample(3).times
+  end
+
+  context "works exponential" do
+    it {
+      expect { |n, _i|
+        prepare_data(n) { |filename| work(filename) }
+      }.to perform_exponential.in_range(10, 10_000)
+    }
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,24 @@
 require 'rspec-benchmark'
+require 'fileutils'
 
 RSpec.configure do |config|
   config.include RSpec::Benchmark::Matchers
+
+  def fixtures_path
+    File.expand_path(File.join(__dir__, 'fixtures'))
+  end
+
+  def root_path
+    File.expand_path(File.join(__dir__, '../'))
+  end
+
+  def prepare_data(data_size)
+    filename = "#{fixtures_path}/sample.txt"
+
+    `head -n #{data_size} #{root_path}/data_large.txt > #{filename}`
+
+    yield filename
+  ensure
+    FileUtils.rm(filename)
+  end
 end

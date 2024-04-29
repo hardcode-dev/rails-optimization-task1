dca76721b2530015e685ce7a4e3c2b78792dc3ab
Nikita Kononov <vocrsz@gmail.com>
2024-04-29 20:13:56 +0200

    finish with rbspy

--- test.rb
+++ test.rb
@@ -1,9 +1,14 @@
+#!/usr/bin/env ruby
+
 require_relative 'task-1.rb'
+# require 'bundler/setup'
 
-require 'benchmark'
+# require 'benchmark'
 
-Benchmark.bm do |x|
-  x.report { work('sample100.txt') }
-  x.report { work('sample1000.txt') }
-  x.report { work('sample10000.txt') }
-end
+# Benchmark.bm do |x|
+#   x.report { work('sample100.txt') }
+#   x.report { work('sample1000.txt') }
+#   x.report { work('sample10000.txt') }
+# end
+
+work('data_large.txt')

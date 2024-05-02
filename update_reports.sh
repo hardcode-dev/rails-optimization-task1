b04fcafff90526fa6451c91a51c67c98d6139e44
Kᴏɴsᴛᴀɴᴛɪɴ Sʜɪʟᴏᴠ <shilovk@gmail.com>
2024-05-02 17:50:37 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	update_reports.sh

--- update_reports.sh
+++ update_reports.sh
@@ -4,7 +4,7 @@ DATA_FILE='data200_000.txt' DISABLE_GC=false ruby task-1_ruby_prof_flat.rb
 DATA_FILE='data200_000.txt' DISABLE_GC=false ruby task-1_ruby_prof_graph.rb
 DATA_FILE='data200_000.txt' DISABLE_GC=false ruby task-1_ruby_prof_callstack.rb
 DATA_FILE='data200_000.txt' DISABLE_GC=false ruby task-1_ruby_prof_callgrind.rb
+DATA_FILE='data200_000.txt' DISABLE_GC=false ruby task-1_ruby_prof_flamegraph.rb
 DATA_FILE='data200_000.txt' DISABLE_GC=false ruby task-1_stackprof_cli.rb
 DATA_FILE='data200_000.txt' DISABLE_GC=false ruby task-1_stackprof_flamegraph.rb
-DATA_FILE='data200_000.txt' DISABLE_GC=true ruby task-1_ruby_prof_flamegraph.rb
 ruby task-1_test.rb

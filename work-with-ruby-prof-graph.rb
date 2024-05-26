a400fa712da5ede18c5d3d00f1ecf081247be6c9
Diana Shchegoleva <dschegoleva@gmail.com>
2024-05-26 12:33:11 +0400

    Optimize sessions select by user_id

---
+++ work-with-ruby-prof-graph.rb
@@ -0,0 +1,15 @@
+require 'ruby-prof'
+require_relative 'task-1'
+
+profile = RubyProf::Profile.new
+RubyProf.measure_mode = RubyProf::WALL_TIME
+puts "Start work"
+
+GC.disable
+result = profile.profile do
+  work
+end
+
+printer = RubyProf::GraphHtmlPrinter.new(result)
+printer.print(File.open('ruby_prof_reports/graph1.html', 'w+'))
+puts "End work"
